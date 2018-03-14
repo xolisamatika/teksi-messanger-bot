fa1835f4f944f88a8346841f1eafe2496055cd21
Xolisa Matika <xolisa.matika@bettech.com>
2018-03-14 11:45:34 +0200

    Fixed the geocoder.

--- index.js
+++ index.js
@@ -25,17 +25,14 @@ const sslOptions = {
 const app = express().use(bodyParser.json());
 // Sets server port and logs message on success
 https.createServer(sslOptions, app).listen(process.env.PORT || 1337, () => {
-
-  console.log('webhook is listening');
-  const res = getAddressByCoordinates({lat: -33.92947, lon:18.41071});
-  console.log(res);
-
+// app.listen(process.env.PORT || 1337, () => {
+    console.log('webhook listening....');
 });
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Creates the endpoint for our webhook 
-app.post('/tmb/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {
  
     let body = req.body;
 
@@ -72,7 +69,7 @@ app.post('/tmb/webhook', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/tmb/webhook', (req, res) => {
+app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
@@ -99,117 +96,11 @@ app.get('/tmb/webhook', (req, res) => {
     }
   });
 
-  // Handles messages events
 const handleMessage = async (sender_psid, received_message) => {
-  let response;
-  
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `Hello, would you like a ride?`,
-      "quick_replies":[
-        {
-          "content_type":"text",
-          "title":"Yes",
-            "payload": { "need_ride": {
-              "response_text": "yes"
-            }
-          }
-        },
-        {
-          "content_type":"text",
-          "title":"No",
-          "payload": {
-            "need_ride": {
-              "response_text": "no"
-            }
-          }
-        }
-      ]
-    };
-  } else if (received_message.attachments) {
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic"
-        }
-      }
-    };
 
-    if(received_message.attachments[0].payload.coordinates){
-      const results = await getAddressByCoordinates(received_message.attachments[0].payload.coordinates);
-      console.log(results);
-      let title = `Sorry, I can't get you street address from the shared location, Please type in your Street Address`;
-      let buttons = [];
-      if(results.length == 1){
-        title = `Is this the correct address?`;
-        buttons = [
-            {
-              "type": "postback",
-              "title": "Yes!",
-              "payload": {"street_address_correct": true},
-            },
-            {
-              "type": "postback",
-              "title": "No!",
-              "payload": {"street_address_correct": false},
-            }
-        ];
-      } else if (results.length > 1) {
-        results.forEach(element => {
-        title = `Please select the correct address below :`;
-          buttons.push(
-            {
-            "type": "postback",
-            "title": `${element}`,
-            "payload": {"street_address": element},
-            });
-        });
-      }
-      response.elements = {"title": title, "buttons": buttons};
-    } else if(received_message.attachments[0].payload.need_ride) {
-      let need_ride = received_message.attachments[0].payload.need_ride;
-      if (need_ride == "yes") {
-        response = null;
-        response = {
-          "text": `Please share your location:`,
-          "quick_replies":[
-            {
-              "content_type":"location"
-            }
-          ]
-        };
-      } else {
-        response = {"message":{
-            "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"button",
-                "text":"What do you want to do next?",
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://www.teksi.co.za",
-                    "title":"Visit our site"
-                  },
-                  {
-                    "type":"phone_number",
-                    "title":"Call one of our drivers",
-                    "payload":"+15105551234"
-                  }
-                ]
-              }
-            }
-        }
-      };
-    }
-  }
-  
-  // Send the response message
-  await callSendAPI(sender_psid, response);    
+    const response = await buildResponse(received_message);
+
+    await callSendAPI(sender_psid, response);    
 }
 
 // Handles messaging_postbacks events
@@ -217,54 +108,138 @@ const handlePostback = async (sender_psid, received_postback) => {
     let response;
     
     // Get the payload for the postback
-    let payload = await received_postback.payload;
+    const payload = received_postback.payload;
 
     // Set the response based on the postback payload
     if (payload === 'yes') {
-      response = { "text": "Thanks!" }
+        response = { "text": "Thanks!" }
     } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
+        response = { "text": "Oops, try sending another image." }
     }
-    // Send the message to acknowledge the postback
     await callSendAPI(sender_psid, response);
 }
 
-// Sends response messages via the Send API
 const callSendAPI = async (sender_psid, response) => {
-    // Construct the message body
     let request_body = await {
-      "recipient": {
+        "recipient": {
         "id": sender_psid
-      },
-      "message": response
-    }
-    console.log(request_body);
-    /*
-    // Send the HTTP request to the Messenger Platform
-    await request({
-      "url": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
-    }, (err, res, body) => {
-      console.log('err : ',err);
-      console.log('res : ', res);
-      console.log('body : ',body);
+        },
+        "message": response
+    };
 
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }); */
+    await request({
+        "url": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    console.log(request_body);
 }
 
 const getAddressByCoordinates = async (coordinates) => {
-  try {
-    const results = await geocoder.reverse(coordinates);
-    console.log(results);
-    return results.map(result => result.formattedAddress);
-  } catch(err){
-    console.error(err) 
-  }
+    try {
+        const results = await geocoder.geocode(`${coordinates.lat}, ${coordinates.long}`);
+        return results.map(result => result.formattedAddress);;
+    } catch(err){
+        console.error(err) 
+    }
 }
+
+const buildResponse = async (request) => {
+    let response;
+
+    console.log(request);
+    if (request.text) {
+        response = await {
+            "text": `Hello, would you like a ride?`
+        };
+        
+    } else if (request.attachments) {
+        if(request.attachments[0].payload.coordinates){
+            const results = await getAddressByCoordinates(request.attachments[0].payload.coordinates);
+            console.log(results);
+            let title = `Sorry, I can't get you street address from the shared location, Please type in your Street Address`;
+            let buttons = [];
+            if(results.length == 1){
+                title = `Is this the correct address?`;
+                buttons = [
+                    {
+                    "type": "postback",
+                    "title": "Yes!",
+                    "payload": {"street_address_correct": true},
+                    },
+                    {
+                    "type": "postback",
+                    "title": "No!",
+                    "payload": {"street_address_correct": false},
+                    }
+                ];
+            }
+        }
+    }
+    return response;
+}
+
+
+//   /*
+//   if (received_message.attachments) {
+//         response = {
+//             "attachment": {
+//                 "type": "template",
+//                 "payload": {"template_type": "generic"}
+//             }
+//         };
+
+//       if (results.length > 1) {
+//           results.forEach(element => {
+//           title = `Please select the correct address below :`;
+//             buttons.push(
+//               {
+//               "type": "postback",
+//               "title": `${element}`,
+//               "payload": {"street_address": element},
+//               });
+//           });
+//         }
+//         response.elements = {"title": title, "buttons": buttons};
+//       } else if(received_message.attachments[0].payload.need_ride) {
+//         let need_ride = received_message.attachments[0].payload.need_ride;
+//         if (need_ride == "yes") {
+//             response = null;
+//             response = {
+//                 "text": `Please share your location:`,
+//                 "quick_replies":[{
+//                     "content_type":"location"
+//                 }]
+//             };
+//         } else {
+//             response = {
+//                 "message":{
+//                     "attachment":{
+//                         "type":"template",
+//                         "payload":{
+//                             "template_type":"button",
+//                             "text":"What do you want to do next?",
+//                             "buttons":[{
+//                                 "type":"web_url",
+//                                 "url":"https://www.teksi.co.za",
+//                                 "title":"Visit our site"
+//                             },
+//                             {
+//                                 "type":"phone_number",
+//                                 "title":"Call one of our drivers",
+//                                 "payload":"+15105551234"
+//                             }]
+//                         }
+//                     }
+//                 }
+//             };
+//         }
+//     }
+//     }
